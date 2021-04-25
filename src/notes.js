e25c74be60444d6f1283ebf2c5f4a49c2a1bcd55
Maulik Shah <maulik.shah715@gmail.com>
2021-04-25 12:35:19 +0530

    📚 Added some default notes

---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    key: 1,
+    title: "This is note 1",
+    content: "This is note 1 content",
+  },
+  {
+    key: 2,
+    title: "This is note 2",
+    content: "This is note 1 content",
+  },
+  {
+    key: 3,
+    title: "This is note 3",
+    content: "This is note 1 content",
+  },
+  {
+    key: 4,
+    title: "This is note 4",
+    content: "This is note 4 content",
+  },
+  {
+    key: 5,
+    title: "This is note 5",
+    content: "This is note 5's content",
+  },
+];
+
+export default notes;
